c0445466d8b748945d45a1d2b0df373f5b177447
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-12-17 11:49:54 +0000

    feat: updated the api.php file to include patient, doctor and admin login pages

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+Route::post('/loginPatient', [AuthController::class, 'patientLogin']);
+Route::post('/loginDoctor', [AuthController::class, 'doctorLogin']);
+Route::post('/loginAdmin', [AuthController::class, 'adminLogin']);
 Route::middleware('auth:sanctum')->group(function () {
    Route::post('/registerPatient', [AuthController::class, 'patientRegister']);
    Route::post('/registerDoctor', [AuthController::class, 'doctorRegister']);
