e2b6126ac736693583db7fb13e4a3c920c3bd6e8
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-12-17 21:16:19 +0000

    chore: added vue dependecny and also set up the web.php

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <router-view />
+</template>
+<script>
+export default {
+    name: "App",
+};
+</script>
+<style>
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #D22B2B;
+}
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from 'vue-router';
+import { createPinia } from 'pinia';
+
+const app = createApp(App)
+const pinia = createPinia();
+
+app.mount('#app');
