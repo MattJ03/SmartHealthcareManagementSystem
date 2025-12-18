8dc4999d24526d3bd4d3515a4c909155dc946506
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-12-18 18:22:36 +0000

    fix: cleaned up the authstore

--- resources/js/stores/AuthStore.js
+++ resources/js/stores/AuthStore.js
@@ -7,6 +7,7 @@ import api from "../axios.js";
 export const useAuthStore = defineStore('auth', () => {
    const token = ref(localStorage.getItem('token'));
    const user = ref(null);
+   const role = ref(localStorage.getItem('role'));
    const loading = ref(false);
    const error = ref('');
 
@@ -15,7 +16,9 @@ export const useAuthStore = defineStore('auth', () => {
        try {
            const res = await api.post('login', {email, password});
            token.value = res.data.token;
+           role.value = res.data.role;
            localStorage.setItem('token', token.value);
+           localStorage.setItem('role', role.value);
        } catch (error) {
            console.log(error.response?.data || error.message);
        } finally {
@@ -23,12 +26,43 @@ export const useAuthStore = defineStore('auth', () => {
        }
    }
 
-   async function patientRegister(form) {
-       loading.value = true;
-       try {
-           const res = await api.post('patientRegister')
-       }
+   async function patientRegister(payload) {
+       return register('/registerPatient', payload);
    }
 
+   async function doctorRegister(payload) {
+       return register('/registerDoctor', payload)
+   }
+
+   async function adminRegister(payload) {
+       return register('/registerAdmin', payload);
+   }
+
+   async function register(endpoint, payload) {
+       loading.value = true;
+       try {
+           const res = await api.post(endpoint, payload);
+           return res.data;
+       } catch (error) {
+           error.value = error.response?.data?.message || 'Registration failed';
+           throw error;
+       } finally {
+           loading.value = false;
+
+       }
+    }
+
+    return [
+      token,
+      user,
+      role,
+      loading,
+      error,
+      login,
+      register,
+      patientRegister,
+      doctorRegister,
+      adminRegister,
+    ];
 
 });
