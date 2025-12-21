5f215e68ab2333145ae71ccafb2e0103af7d635e
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-12-21 23:55:25 +0000

    feat: added the patient and doctor images and have centered them both on the screen choose new doctor image

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -3,12 +3,14 @@ import RegisterPatient from '../screens/RegisterPatient.vue';
 import Login from "../screens/Login.vue";
 import Home from "../screens/Home.vue";
 import RegisterAdmin from "../screens/RegisterAdmin.vue";
+import RegisterSelect from "../screens/RegisterSelect.vue";
 
 const routes = [
     { path: '/login', component: Login },
     { path: '/home', component: Home },
     { path: '/register-patient', component: RegisterPatient, meta: {role: 'admin'}},
     { path: '/register-admin', component:RegisterAdmin, meta: {role: 'admin'}},
+    { path: '/register-select', component: RegisterSelect, meta: {role: 'admin'}},
 
 ];
 
---
+++ resources/js/screens/RegisterSelect.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="block">
+    <div class="patient-img-container">
+        <img :src="patientIcon" alt="patientImage" class="patient-img" />
+    </div>
+    <div class="doctor-img-container">
+        <img :src="doctorIcon" alt="doctorImage" class="doctor-img" />
+    </div>
+    </div>
+</template>
+<script setup>
+import { ref, reactive, computed } from "vue";
+import patientIcon from '../assets/patienticon.webp';
+import doctorIcon from '../assets/newDoctor.png';
+import adminIcon from '../assets/adminicon.webp';
+
+
+</script>
+<style scoped>
+.block {
+    display: flex;
+    width: 100%;
+    gap: 300px;
+
+}
+.patient-img-container {
+    height: 100vh;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    width: 20%;
+}
+.patient-img {
+    height: 400px;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    border: none;
+}
+.doctor-img-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.doctor-img {
+    height: 400px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+}
+
+</style>
