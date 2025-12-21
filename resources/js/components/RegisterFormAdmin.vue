5ced73fff2ae08a4fd1b617729a30be94364096d
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-12-21 19:30:20 +0000

    feat: created the form for reg admin

---
+++ resources/js/components/RegisterFormAdmin.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+<script setup>
+import { ref, reactive, computed} from "vue";
+import pill from '../assets/pill.PNG'
+
+</script>
+<style scoped>
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem;
+    border: none;
+    border-radius: 14px;
+    height: auto;
+    min-height: 400px;
+    width: 350px;
+    max-width: 800px;
+    background-color: #F2F0EF;
+}
+.form {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: flex-start;
+    gap: 2rem;
+}
+</style>
