41c96451db979b34e5afa83e0f416a9a815dee05
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-12-22 00:14:49 +0000

    feat: added the admin picture and added margin left to create a bit of space

--- resources/js/screens/RegisterSelect.vue
+++ resources/js/screens/RegisterSelect.vue
@@ -6,6 +6,9 @@
     <div class="doctor-img-container">
         <img :src="doctorIcon" alt="doctorImage" class="doctor-img" />
     </div>
+        <div class="admin-img-container">
+            <img :src="adminIcon" alt="adminImage" class="admin-img" />
+        </div>
     </div>
 </template>
 <script setup>
@@ -21,7 +24,6 @@ import adminIcon from '../assets/adminicon.webp';
     display: flex;
     width: 100%;
     gap: 300px;
-
 }
 .patient-img-container {
     height: 100vh;
@@ -29,6 +31,7 @@ import adminIcon from '../assets/adminicon.webp';
     justify-content: left;
     align-items: center;
     width: 20%;
+    margin-left: 20px;
 }
 .patient-img {
     height: 400px;
@@ -43,6 +46,18 @@ import adminIcon from '../assets/adminicon.webp';
     align-items: center;
 }
 .doctor-img {
+    height: 400px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+}
+.admin-img-container {
+    display: flex;
+    justify-content: right;
+    align-items: center;
+}
+.admin-img {
     height: 400px;
     display: flex;
     justify-content: center;
