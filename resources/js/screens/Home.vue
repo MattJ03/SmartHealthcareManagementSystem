e7b6cd46072817ce26219e3b6faab0797a371559
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-12-27 22:53:34 +0000

    feat: alligned items

--- resources/js/screens/Home.vue
+++ resources/js/screens/Home.vue
@@ -1,11 +1,14 @@
 <template>
     <NavBar></NavBar>
     <div class="welcome-container">
-        <h1 class="welcome-name">Welcome back {{ name }}</h1>
+        <h1 class="welcome-name">Welcome back, {{ name }}</h1>
         <p class="welcome-name">How are feeling today?</p>
     </div>
     <div class="next-appointment-container">
+        <div class="next-appoint-clock">
+        <img :src="clock" class="img" alt="clock" />
        <h2>Next Appointment</h2>
+        </div>
         <button class="view-details-btn">View Details</button>
     </div>
 
@@ -17,6 +20,7 @@ import { ref, reactive, computed } from 'vue';
 import router from "../router/index.js";
 import { useAuthStore } from "../stores/AuthStore.js";
 import { storeToRefs } from "pinia";
+import clock from '../assets/clock.png';
 
 const store = useAuthStore();
 
@@ -26,7 +30,7 @@ const { name } = storeToRefs(store);
 </script>
 <style style>
 .welcome-container {
-   padding-left: 30px;
+   padding-left: 120px;
     padding-right: 20px;
     padding-top: 15px;
 }
@@ -43,11 +47,17 @@ const { name } = storeToRefs(store);
 .next-appointment-container {
     display: flex;
     align-items: center;
+    margin: auto;
+
     justify-content: space-between;
+
     background-color: #C0392B;
     color: #FFFFFF;
-    width: 65%;
+    width: 85%;
     padding: 20px;
+    border-radius: 14px;
+    margin-top: 30px;
+
 
 }
 
@@ -65,6 +75,15 @@ const { name } = storeToRefs(store);
     transition-duration: 300ms;
     cursor: pointer;
 }
+.img {
+    height: 25px;
+}
 
+.next-appoint-clock {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 15px;
+}
 
 </style>
