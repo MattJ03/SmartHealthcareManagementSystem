fc71a54e30d35aabf029f34712baefcfbd89107d
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-12-27 22:31:23 +0000

    feat: added the contrianer for the next appointment and the view button

--- resources/js/screens/Home.vue
+++ resources/js/screens/Home.vue
@@ -4,6 +4,10 @@
         <h1 class="welcome-name">Welcome back {{ name }}</h1>
         <p class="welcome-name">How are feeling today?</p>
     </div>
+    <div class="next-appointment-container">
+       <h2>Next Appointment</h2>
+        <button class="view-details-btn">View Details</button>
+    </div>
 
 
 </template>
@@ -23,7 +27,7 @@ const { name } = storeToRefs(store);
 <style style>
 .welcome-container {
    padding-left: 30px;
-
+    padding-right: 20px;
     padding-top: 15px;
 }
 .welcome-name {
@@ -35,4 +39,32 @@ const { name } = storeToRefs(store);
     font-size: 20px;
      margin-top: 10px;
 }
+
+.next-appointment-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #C0392B;
+    color: #FFFFFF;
+    width: 65%;
+    padding: 20px;
+
+}
+
+.view-details-btn {
+    padding-bottom: 15px;
+    padding-top: 15px;
+    padding-left: 30px;
+    padding-right: 30px;
+    background-color: #FFFFFF;
+    border-radius: 14px;
+    font-size: 16px;
+}
+.view-details-btn:hover {
+    background-color: #f0f0f0;
+    transition-duration: 300ms;
+    cursor: pointer;
+}
+
+
 </style>
