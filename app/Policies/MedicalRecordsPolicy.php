037d03423a5a1e0fbe51b6fb29face25f33ed007
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-01-09 23:32:27 +0000

    feat: created the 'any' policy method to allow both the user and patient doctor to view the files

---
+++ app/Policies/MedicalRecordsPolicy.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\MedicalRecord;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+use App\Models\PatientProfile;
+
+
+class MedicalRecordsPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, MedicalRecord $medicalRecord): bool
+    {
+        return $user->patient_profile?->doctor_id === $medicalRecord->doctor_id || $user->id === $medicalRecord->patient_id;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, MedicalRecord $medicalRecord): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, MedicalRecord $medicalRecord): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, MedicalRecord $medicalRecord): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, MedicalRecord $medicalRecord): bool
+    {
+        return false;
+    }
+}
