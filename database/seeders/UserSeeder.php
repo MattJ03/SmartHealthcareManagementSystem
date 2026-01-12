9a91f44affda7530a12c137d54990294527750ca
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-01-12 20:14:14 +0000

    feat: created userseeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Database\Factories\UserFactory;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->count(5)->create()->each(function ($user) {
+            $user->assignRole('patient');
+        });
+
+        User::factory()->count(5)->create()->each(function ($user) {
+            $user->assignRole('doctor');
+        });
+
+        $admin = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password123'),
+        ]);
+    }
+}
