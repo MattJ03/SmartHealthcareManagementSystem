2ee761b26f6829212360c6c0ee7becaf2bf59c1d
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-01-13 19:38:09 +0000

    feat: setting up the records grid comp

---
+++ resources/js/components/MedicalHistoryGrid.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="record-grid">
+            <p> {{}}</p>
+
+        </div>
+
+    </div>
+</template>
+<script setup>
+import { ref, reactive, computed } from "vue";
+
+const props = defineProps({
+    appointment: {
+        type: Object,
+        required: true,
+    }
+});
+
+const appointmentRef = ref(props.appointment);
+
+</script>
+<style scoped>
+.container {
+    width: 100%;
+    background-color: #FFFFFF;
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+}
+.record-grid {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 20%;
+    border: 1px solid #FFFFFF;
+    background-color: #C0392B;
+}
+</style>
--- resources/js/screens/MedicalRecords.vue
+++ resources/js/screens/MedicalRecords.vue
@@ -1,7 +1,29 @@
 <template>
-<h1>Waddupt niggs</h1>
+    <NavBar></NavBar>
+    <div class="container">
+      <MedicalHistoryGrid>
+
+      </MedicalHistoryGrid>
+    </div>
+
 </template>
 <script setup>
+import { ref, reactive, computed } from 'vue';
+import NavBar from "../components/NavBar.vue";
+import MedicalHistoryGrid from "../components/MedicalHistoryGrid.vue";
+
 </script>
 <style scoped>
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #FFFFFF;
+    width: 85%;
+    height: 400px;
+    margin: auto;
+    border-radius: 14px;
+    border: 1px solid #E9DCC9;
+    margin-top: 30px;
+}
 </style>
