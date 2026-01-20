880ee884ef5d9ff8e840b4fdbf14a0cf947ec879
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-01-20 17:21:28 +0000

    feat: added update and delete buttons to doctors appointment grid

--- resources/js/components/DoctorUpcomingAppointmentsGrid.vue
+++ resources/js/components/DoctorUpcomingAppointmentsGrid.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="container">
         <div class="appointment-card">
+            <div class="details">
             <p class="patient-name"> {{ appointment.patient?.name ?? 'Loading...'}}</p>
             <p class="starts-at"> {{ appointmentDate ?? 'Loading...' }}</p>
             <p class="starts-at"> {{ appointmentTime }} {{ appointmentPeriod }}</p>
+        </div>
+            <div class="delete-section">
+                <button class="delete-btn" @click="$emit('delete', appointment.id)"> Cancel </button>
+                <button class="update-btn" @click="$emit('update', appointment.id)"> Update </button>
+            </div>
         </div>
 
     </div>
@@ -19,25 +25,66 @@ const props = defineProps({
     },
 });
 
+const emit = defineEmits(['delete', 'update']);
 
-const { appointmentDate, appointmentTime, appointmentPeriod } = useFormattedAppointment(props.appointment);
+const appointmentRef = ref(props.appointment)
+const { appointmentDate, appointmentTime, appointmentPeriod } = useFormattedAppointment(appointmentRef);
 
 </script>
 <style scoped>
 .container {
     background-color: #FFFFFF;
+    width: 100%;
 }
 .appointment-card {
     display: flex;
     justify-content: left;
-    flex-direction: column;
+    flex-direction: row;
     border: 1px solid #E9DCC9;
     border-radius: 14px;
+    padding-right: px;
 }
 .starts-at {
     font-size: 18px;
 }
 .patient-name {
     font-size: 16px;
+}
+.delete-section {
+    display: flex;
+    align-items: center;
+    gap: 15px;
+}
+.details {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 20px;
+    flex: 1;
+}
+
+.delete-btn {
+    padding-bottom: 15px;
+    padding-top: 15px;
+    padding-left: 25px;
+    padding-right: 25px;
+    background-color: #C0392B;
+    border-radius: 14px;
+    font-size: 16px;
+    color: #FFFFFF;
+    border: 0;
+
+}
+
+.update-btn {
+    padding-bottom: 15px;
+    padding-top: 15px;
+    padding-left: 25px;
+    padding-right: 25px;
+    background-color: #C0392B;
+    border-radius: 14px;
+    font-size: 16px;
+    background-color: #FFBF00;
+    border: 0;
 }
 </style>
