ae6542eae78941ad23d770a5875988128901ca88
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-03-27 16:36:04 +0000

    feat: created the get all logs and get patient logs store methods

--- resources/js/stores/ActivityLogsStore.js
+++ resources/js/stores/ActivityLogsStore.js
@@ -4,5 +4,36 @@ import api from "../axios.js";
 
 export const useActivityLogsStore = defineStore('activity_logs', () => {
     const role = localStorage.getItem('role');
-    const
-})
+    const allLogs = ref([]);
+    const patientLogs = ref([]);
+    const doctorLogs = ref([]);
+    const loading = ref(false);
+    const error = ref(null);
+
+    const getAllLogs = async () => {
+        loading.value = true;
+        try {
+            const res = await api.get('/getCompleteLogList');
+            allLogs.value = res.data.data;
+        } catch (error) {
+            error.value = error.response?.data?.message ?? 'Failed to fetch all logs';
+        }
+        finally {
+            loading.value = false;
+        }
+    }
+
+    const getPatientLogs = async () => {
+        loading.value = true;
+        try {
+            const res = await api.get('getPatientsLogList');
+            patientLogs.value = res.data.data;
+        } catch(error) {
+            error.value = error.response?.data?.message ?? 'Failed to fetch patient logs';
+        }
+        finally {
+            loading.value = false;
+        }
+    }
+
+});
