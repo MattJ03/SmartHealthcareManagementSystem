32a5ee3d26d3cd5a21b9a85bdb3714c820a4fe11
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-04-01 23:01:40 +0100

    feat: set up the log props

---
+++ resources/js/components/LogGrid.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="log-rec">
+
+        </div>
+    </div>
+</template>
+<script setup>
+import { ref, reactive, computed } from 'vue';
+
+const props = defineProps({
+    logs: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
+<style scoped>
+.container {
+    background-color: #FFFFFF;
+    width: 100%;
+}
+.log-rec {
+    display: flex;
+    justify-content: left;
+}
+</style>
--- resources/js/screens/Alerts.vue
+++ resources/js/screens/Alerts.vue
@@ -1,10 +1,14 @@
 <template>
+    <NavBar></NavBar>
 <h1>Make this a log thing for bookings, postings, doctor moves, patient cancel or new patient</h1>
 </template>
 <script setup>
 import { ref, reactive, computed } from 'vue';
 import { useActivityLogsStore } from "../stores/ActivityLogsStore.js";
 import { storeToRefs } from 'pinia';
+import LogGrid from "../components/LogGrid.vue";
+import NavBar from "../components/NavBar.vue";
+
 
 const logsStore = useActivityLogsStore();
 
