81a46cf217fac1279d8bab4ce92a28e52fdc94d2
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-04-01 23:56:20 +0100

    feat: doctor fetch for doctor role

--- resources/js/screens/Alerts.vue
+++ resources/js/screens/Alerts.vue
@@ -7,6 +7,12 @@
      :key="log.id"
      :logs="log"
     />
+    <LogGrid
+    v-if="role === 'doctor'"
+    v-for="log in doctorLogs"
+    :key="log.id"
+    :logs="log"
+    />
 </template>
 <script setup>
 import { ref, reactive, computed } from 'vue';
@@ -19,11 +25,19 @@ import {useAuthStore} from "../stores/AuthStore.js";
 
 const logsStore = useActivityLogsStore();
 const authStore = useAuthStore();
+
 const { role } = storeToRefs(authStore);
+const { patientLogs, doctorLogs, allLogs } = storeToRefs(logsStore);
 
 onMounted (() => {
     if(role.value === 'patient') {
         logsStore.getPatientLogs();
+    }
+    if(role.value === 'doctor') {
+        logsStore.getDoctorLogs();
+    }
+    if(role.value === 'admin') {
+        logsStore.getAllLogs();
     }
 
 })
