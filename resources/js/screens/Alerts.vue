48950c3b2000f0f9bb51849c0c3b787dbba8487b
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-04-01 22:42:53 +0100

    feat: created the log component

--- resources/js/screens/Alerts.vue
+++ resources/js/screens/Alerts.vue
@@ -2,6 +2,14 @@
 <h1>Make this a log thing for bookings, postings, doctor moves, patient cancel or new patient</h1>
 </template>
 <script setup>
+import { ref, reactive, computed } from 'vue';
+import { useActivityLogsStore } from "../stores/ActivityLogsStore.js";
+import { storeToRefs } from 'pinia';
+
+const logsStore = useActivityLogsStore();
+
+
+
 </script>
 <style scoped>
 </style>
