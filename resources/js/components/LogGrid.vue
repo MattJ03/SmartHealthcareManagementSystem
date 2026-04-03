0177d87d0836cbae137835a5f704b4c823d10371
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-04-03 18:25:39 +0100

    feat: formatting the logs and converting the UTC time to normal dates using DateTime class

--- resources/js/components/LogGrid.vue
+++ resources/js/components/LogGrid.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="container">
         <div class="log-rec">
+            <p class="log-details"> {{ formattedTime }}</p>
+            <p class="log-details"> {{ logs.action }}</p>
             <p class="log-details"> {{ logs.description }}</p>
         </div>
     </div>
@@ -14,22 +16,27 @@ const props = defineProps({
         required: true,
     },
 });
+
+const date = new Date();
+const formattedTime = date.toLocaleDateString(props.created_at)
 </script>
 <style scoped>
 .container {
     background-color: #FFFFFF;
-    width: 75%;
+    width: 60%;
     padding-left: 40px;
 }
 .log-rec {
-    display: flex;
-    justify-content: left;
+    display: grid;
+    grid-template-columns: 1fr 1fr 2fr;
     border: 1px solid #FFFFFF;
     background-color: #0a53be;
-    padding-left: 20px;
+
     border-radius: 14px;
 }
 .log-details {
     font-size: 16px;
+    padding-left: 10px;
+    padding-right: 10px;
 }
 </style>
--- resources/js/screens/Alerts.vue
+++ resources/js/screens/Alerts.vue
@@ -3,10 +3,28 @@
     <div class="container">
         <div class="log-container">
         <div class="log-headings">
-            <h2>hello</h2>
-            <h2>goodbye</h2>
-            <h2>question</h2>
+            <h3>Timestamp</h3>
+            <h3>Action</h3>
+            <h3>Description</h3>
         </div>
+            <div class="horizontal-line">
+                <hr>
+            </div>
+            <div class="log-results">
+                <LogGrid
+                    v-if="role === 'patient'"
+                    v-for="log in patientLogs"
+                    :id="log.id"
+                    :logs="log"
+                    />
+                <LogGrid
+                    v-if="role === 'doctor'"
+                    v-for="log in doctorLogs"
+                    :id="log.id"
+                    :logs="log"
+                    />
+            </div>
+
         </div>
     </div>
 </template>
@@ -43,17 +61,34 @@ onMounted (() => {
     display: flex;
     justify-content: left;
     width: 100%;
+    padding-left: 30px;
 }
 .log-container {
-    display: flex;
-    justify-content: space-between;
+
     gap: 50px;
-    flex-direction: row;
-    background-color: #0a53be;
+    padding-left: 0;
+    background-color: #FFFFFF;
+    border: #E9DCC9 solid 1px;
     border-radius: 14px;
-    width: 50%;
+    width: 75%;
+    height: 400px;
 }
 .log-headings {
+    display: grid;
+    padding-left: 20px;
+    grid-template-columns: 1fr 1fr 2fr;
+    width: 100%;
+}
+
+.horizontal-line {
+    width: 100%;
+    border: none;
+
+    margin: 10px 0;
+    padding-left: 0;
+}
+
+.log-results {
 
 }
 </style>
