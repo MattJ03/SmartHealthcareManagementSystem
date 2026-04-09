1040a50af5800696db02f0d715032f78eb1baa7c
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-04-09 17:18:11 +0100

    feat: added button going over styling first

--- resources/js/components/PatientList.vue
+++ resources/js/components/PatientList.vue
@@ -2,9 +2,9 @@
     <div class="container">
         <div class="patient-info-container">
             <p class="patient-name"> {{ props.patient.user.name }}</p>
+            <button class="view-details">WAGWAN</button>
         </div>
     </div>
-
 </template>
 <script setup>
 import { ref, reactive, computed } from 'vue';
@@ -21,8 +21,35 @@ const props = defineProps({
 <style scoped>
 .container {
     display: flex;
+    width: 100%;
+
 }
 .patient-info-container {
-
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+   background-color: #FFFFFF;
+    padding: 0;
+    border-radius: 14px;
+    padding-left: 18px;
+    padding-top: 8px;
+    padding-right: 18px;
+    padding-bottom: 8px;
+}
+.patient-name {
+    font-size: 18px;
+}
+.view-details {
+    padding-bottom: 12px;
+    padding-top: 12px;
+    padding-left: 22px;
+    padding-right: 22px;
+    background-color: #C0392B;
+    border-radius: 14px;
+    font-size: 16px;
+    background-color: #FFBF00;
+    color: #FFFFFF;
+    border: 0;
 }
 </style>
--- resources/js/screens/MyPatients.vue
+++ resources/js/screens/MyPatients.vue
@@ -1,13 +1,13 @@
 <template>
 <NavBar></NavBar>
+    <h2 class="header-title">My Patients</h2>
     <div class="container">
-        <h2>My Patients</h2>
+        <div class="patients-info">
        <PatientList
            v-if="role === 'doctor'"
            v-for="patient in patients" :key="patient.id" :patient="patient"
-
            />
-
+        </div>
         </div>
 
 </template>
@@ -33,10 +33,21 @@ onMounted(() => {
 <style scoped>
 .container {
     display: flex;
-    margin: auto;
-    margin-left: 40px;
+   margin-left: 20px;
+    width: 100%;
+
+
 }
 .patients-info {
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+    justify-content: space-between;
+    gap: 5px;
+    border-radius: 14px;
 
+}
+.header-title {
+    margin-left: 20px;
 }
 </style>
