7d071ea61213089f4dc434634363243de54bebec
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-05-03 15:32:16 +0100

    feat: added the lock to the admin records

--- resources/js/screens/AdminMedicalRecords.vue
+++ resources/js/screens/AdminMedicalRecords.vue
@@ -3,7 +3,7 @@
 <div class="container">
    <div class="restricted-information">
       <div class="lock-img-wrapper">
-          <img :src=""
+          <img :src="lock" alt="lock" class="lock-img">
       </div>
    </div>
 
@@ -12,6 +12,7 @@
 </template>
 <script setup>
 import NavBar from "../components/NavBar.vue";
+import lock from '../assets/lock.svg';
 </script>
 <style scoped>
 .container {
@@ -22,8 +23,16 @@ import NavBar from "../components/NavBar.vue";
     display: flex;
      flex-direction: column;
      margin: auto;
+    margin-top: 60px;
 }
 .lock-img-wrapper {
+      height: fit-content;
+    background-color: #e6f1fb;
+    border-radius: 100px;
+    padding: 14px 14px 18px 18px;
+}
+.lock-img {
+    height: 70px;
 
 }
 </style>
