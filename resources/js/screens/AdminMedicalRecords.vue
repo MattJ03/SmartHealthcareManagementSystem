58e168d48cac849746d57dcc1d75ea000e2a0bab
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-05-03 16:17:53 +0100

    feat: info asset and added more to the web page

--- resources/js/screens/AdminMedicalRecords.vue
+++ resources/js/screens/AdminMedicalRecords.vue
@@ -5,6 +5,12 @@
       <div class="lock-img-wrapper">
           <img :src="lock" alt="lock" class="lock-img">
       </div>
+       <span class="access-restricted">Access Restricted</span>
+       <span class="med-records-confidential">Medical records are confidential and can only be accessed by the patient and their assigned doctor.</span>
+       <div class="false-blocking-wrapper">
+           <img :src="info" alt="info" class="info-img">
+           <span class="false-blocking-text">jjjjjjjjjjjjjjjjjjjjjjjjjjjjj</span>
+       </div>
    </div>
 
 </div>
@@ -13,6 +19,7 @@
 <script setup>
 import NavBar from "../components/NavBar.vue";
 import lock from '../assets/lock.svg';
+import info from '../assets/info.svg';
 </script>
 <style scoped>
 .container {
@@ -22,17 +29,39 @@ import lock from '../assets/lock.svg';
 .restricted-information {
     display: flex;
      flex-direction: column;
+
+    align-items: center;
      margin: auto;
-    margin-top: 60px;
+    margin-top: 80px;
 }
 .lock-img-wrapper {
       height: fit-content;
+    width: fit-content;
     background-color: #e6f1fb;
     border-radius: 100px;
     padding: 14px 14px 18px 18px;
 }
 .lock-img {
     height: 70px;
+}
+.access-restricted {
 
+     margin-top: 30px;
+    font-size: 22px;
+
+}
+.med-records-confidential {
+    color: #808080;
+    margin-top: 10px;
+    font-size: 18px;
+}
+.false-blocking-wrapper {
+     height: fit-content;
+    width: fit-content;
+    margin-top: 25px;
+    background-color: #e6f1fb;
+}
+.false-blocking-text {
+    font-size: 16px;
 }
 </style>
