d41dcc84dbd69785f5cb53378e4732de4fdcb23c
Matt Shaw <Shaw-M18@ulster.ac.uk>
2026-05-03 15:09:35 +0100

    feat: added the lock svg to assets

--- resources/js/screens/AdminMedicalRecords.vue
+++ resources/js/screens/AdminMedicalRecords.vue
@@ -1,7 +1,29 @@
 <template>
+    <NavBar></NavBar>
+<div class="container">
+   <div class="restricted-information">
+      <div class="lock-img-wrapper">
+          <img :src=""
+      </div>
+   </div>
+
+</div>
 
 </template>
 <script setup>
+import NavBar from "../components/NavBar.vue";
 </script>
 <style scoped>
+.container {
+    display: flex;
+
+}
+.restricted-information {
+    display: flex;
+     flex-direction: column;
+     margin: auto;
+}
+.lock-img-wrapper {
+
+}
 </style>
